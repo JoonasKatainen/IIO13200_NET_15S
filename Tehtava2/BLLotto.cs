be9b986c73098d06edf1013941d07d2b99ac5c8a
JoonasKatainen <H3248@student.jamk.fi>
2015-09-10 15:17:25 +0300

    Tehtävä 2 palautus
    tehtävä 2 palautus

---
+++ Tehtava2/BLLotto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava2
+{
+    class Lotto
+    {
+
+        public int tyyppi;
+        private int pieninnumero;
+        private int suurinnumero;
+        private int maara;
+        Random r = new Random();
+        public int[] ArvoRivi(int tyyppi)
+        {
+            this.tyyppi = tyyppi;
+            
+            int[] asd;
+            switch (tyyppi) {
+                case(0):
+                    asd = new int[7];
+                    for(int i = 0; i <= 6; i++)
+                    {
+                        asd[i] = r.Next(1, 40);
+                    }
+                    return asd;
+                    break;
+                case (1):
+                    asd = new int[6];
+                    for (int i = 0; i <= 5; i++)
+                    {
+                        asd[i] = r.Next(1, 49);
+                    }
+                    return asd;
+                    break;
+                case (2):
+                    asd = new int[7];
+                    for (int i = 0; i <= 4; i++)
+                    {
+                        asd[i] = r.Next(1, 51);
+                    }
+                    for(int i = 5; i <=6; i++)
+                    {
+                        asd[i] = r.Next(1, 11);
+                    }
+                    return asd;
+                    break;
+                default:
+                    asd = new int[7];
+                    return asd;
+                    break;
+            }
+
+            
+        }
+    }
+}
---
+++ Tehtava2/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void drawbutton_Click(object sender, RoutedEventArgs e)
+        {
+            int arvontojenmaara;
+            Lotto lotto = new Lotto();
+            if (maara.Text == "")
+                arvontojenmaara = 1;
+            else
+                arvontojenmaara = int.Parse(maara.Text);
+           
+            for (int i = 0; i < arvontojenmaara; i++)
+            {
+                int[] rivi = lotto.ArvoRivi(comboBox.SelectedIndex);
+                string srivi = string.Join(" ", rivi);
+                listBox.Items.Add(srivi);
+            }
+        }
+
+        private void comboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> data = new List<string>();
+            data.Add("Suomi");
+            data.Add("VikingLotto");
+            data.Add("EuroJackpot");
+            var comboBox = sender as ComboBox;
+            comboBox.ItemsSource = data;
+            comboBox.SelectedIndex = 0;
+        }
+
+
+        private void clearbutton_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+        }
+
+        private void closebutton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void maara_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);//Tutkii onko syötetty input sallittu
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            Regex regex = new Regex("[^0-9]+"); //Sallii vain numero inputit
+            return !regex.IsMatch(text);
+        }
+    }
+}
